7c15cf3e0704d3a51919911844f46fcfb9015514
dani <dani.zaghian@gmail.com>
2018-05-22 21:45:54 -0700

    reddit solutions

---
+++ reddit_app.rb
@@ -0,0 +1,15 @@
+require "unirest"
+
+system "clear"
+puts "Welcome to the Reddit app!"
+response = Unirest.get("https://www.reddit.com/r/programming.json").body
+
+# puts response['data']['children'][0]['data']['title']
+# puts JSON.pretty_generate(response['data']['children'][0]['data']['title'])
+
+posts = response['data']['children'] #array of posts
+
+posts.each do |post|
+	puts post['data']['title']
+end
+
